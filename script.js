9349e9ecb64f4b5f6dc6d897faed39b790a003b8
cmfernandes <cristianafmfernandes@gmail.com>
2023-05-04 16:54:40 +0100

    create function to add numbers to display

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-let a = 0
-let b = 0
+let number1 = 0 
+let number2 = 0
 let operator = ""; 
+const digits = document.querySelectorAll(".digit")
+const clearBtn = document.querySelector("#clear")
+const equal = document.querySelector("#equalBtn")
+let displayValue = 0
 
-const display = document.querySelector("#display") 
+digits.forEach(button => {
+    button.addEventListener('click',() => {
+         const number = button.dataset.number;
+         addNumber(number);
+    })
+})
+
+function addNumber(number){
+    if (display.textContent === '0') {
+        display.textContent = number;
+    } else {
+        display.textContent += number;
+    }
+}
+clearBtn.addEventListener('click', clear)
+
+ function updateDisplay(){
+    display.textContent = displayValue
+}
+equal.addEventListener('click', () =>{
+    operate(displayValue,operator,displayValue)
+})
+
+addBtn.addEventListener('click', () => {
+    operator = "+";
+    
+})
+
+
+
+oneBtn.addEventListener('click',() => {
+    displayValue = 1;
+    updateDisplay();
+})
+
+function clear() {
+    display.textContent = "0";
+}
 
 function add(a,b){
      
@@ -25,10 +66,10 @@ function divide(a,b){
 }
 
 
-function operate(a,operator,b){
+function operate(a,operator,a){
     switch(operator){
         case "+": 
-            return add(a,b)
+            return add(a,a)
            
         case "-": 
             return subtract(a,b)
