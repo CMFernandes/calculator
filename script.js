d72cf3f02db964049f474c8088ff50cdfec92683
cmfernandes <cristianafmfernandes@gmail.com>
2023-05-07 18:36:40 +0100

    display numbers in currentDisplay

--- script.js
+++ script.js
@@ -1,92 +1,103 @@
-let number1 = 0 
-let number2 = 0
+let previousValue = "";
+let currentValue = "";
 let operator = ""; 
+let currentDisplay = document.querySelector(".current")
+let previousDisplay = document.querySelector(".previous")
+
 const digits = document.querySelectorAll(".digit")
+const operators = document.querySelectorAll(".operaBtn")
+
 const clearBtn = document.querySelector("#clear")
 const equal = document.querySelector("#equalBtn")
-let displayValue = 0
 
-digits.forEach(button => {
-    button.addEventListener('click',() => {
-         const number = button.dataset.number;
-         addNumber(number);
-    })
+const addBtn = document.querySelector("#addBtn")
+const subBtn = document.querySelector("#subBtn")
+const mulBtn = document.querySelector("#multBtn")
+const diviBtn = document.querySelector("#diviBtn")
+
+
+/*equal.addEventListener('click', () =>{
+    currentValue = operate(previousValue,operator,currentValue)
+    previousDisplay.textContent = "0";
+    currentDisplay.textContent = currentValue;
 })
 
-function addNumber(number){
-    if (display.textContent === '0') {
-        display.textContent = number;
-    } else {
-        display.textContent += number;
-    }
-}
-clearBtn.addEventListener('click', clear)
+function clearDisplay() {
+    previousValue = "";
+    currentValue = "";
+    operator = "";
+    currentDisplay.textContent = "0";
+    previousDisplay.textContent = currentValue;
+}*/
+   
 
- function updateDisplay(){
-    display.textContent = displayValue
-}
-equal.addEventListener('click', () =>{
-    operate(displayValue,operator,displayValue)
-})
-
-addBtn.addEventListener('click', () => {
-    operator = "+";
+function add(previousValue,currentValue){
     
-})
-
-
-
-oneBtn.addEventListener('click',() => {
-    displayValue = 1;
-    updateDisplay();
-})
-
-function clear() {
-    display.textContent = "0";
-}
-
-function add(a,b){
-     
-    return a+b;
-}
-
-function subtract(a,b){
+    previousValue += currentValue;
     
-    return a-b;
 }
 
-function multiply(a,b){
-    
-    return a*b;
+function subtract(previousValue,currentValue){
+    return previousValue -= currentValue;
 }
 
-function divide(a,b){
-    
-    return a/b;
+function multiply(previousValue,currentValue){
+    return previousValue *= currentValue;
 }
 
+function divide(previousValue,currentValue){
+    return previousValue /= currentValue;
+}
 
-function operate(a,operator,a){
+function operate(previousValue,operator,currentValue){
+    previousValue = Number(previousValue);
+    currentValue =  Number(currentValue)
     switch(operator){
         case "+": 
-            return add(a,a)
-           
+            return previousValue += currentValue;
         case "-": 
-            return subtract(a,b)
-            
+            return subtract(previousValue,currentValue)
         case "*":
-            return multiply(a,b)
+            return  multiply(previousValue,currentValue)
             
         case "/":
-            return divide(a,b)
+            return  divide(previousValue,currentValue)
     }
-    
+    console.log(previousValue)
 }
 
+digits.forEach(number => number.addEventListener('click', function() {
+    currentValue = number.textContent
+    appendNumber(currentValue)
+}))
+
+/*digits.forEach((number) => number.addEventListener('click', function(){
+        console.log(currentDisplay.textContent.toString())
+        currentValue += currentDisplay.textContent.toString() 
+        appendNumber()
+        console.log(currentValue)
+    }))*/
 
 
+/*operators.forEach(op => op.addEventListener('click', function(e){
+        handleOperator(e.target.textContent)
+        previousDisplay.textContent = previousValue +" "+ operator;
+        //currentDisplay.textContent = currentValue;
+    }))*/
 
+function appendNumber(num){
+    //if (currentValue.length <= 10){
+        currentValue += num
+   // }
+    currentDisplay.textContent = currentValue;
+}
 
+/*function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = "";
+}*/
 
+//clearBtn.addEventListener('click', clearDisplay)
 
 
